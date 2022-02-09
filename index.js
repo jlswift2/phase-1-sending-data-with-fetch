fbf29dfd32aebff4febbda1032cece13b9f92a11
Justin Swift <jlswift2@gmail.com>
2022-02-09 13:02:57 -0600

    commit tree d6a79d3ca2702e9e08e25ad74ffef77b8ea78d9d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(name, email){
+    const formFormat = {
+        "name": name,
+        "email": email,
+    };
+    
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formFormat),
+        }
+    
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(resp  => resp.json())
+    .then(obj => {
+        const goodDiv = document.createElement('div')
+        goodDiv.textContent = obj.id
+        console.log(goodDiv);
+        document.body.appendChild(goodDiv)
+    })
+    .catch(error => {
+        const errorDiv = document.createElement('div')
+        errorDiv.textContent = error.message
+        console.log(errorDiv);
+        document.body.appendChild(errorDiv)
+    });
+}
+
+
+submitData('justin', 'askdljfalsdk')
